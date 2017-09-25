828d167ed48aa335dd5d93f2a60868daea9ad6ea
AssignmentNTU <edwardsujono81@gmail.com>
2017-09-26 01:15:01 +0800

    add data collection

---
+++ dataset_collection/collector_data.py
@@ -0,0 +1,38 @@
+import lxml.etree
+import json
+from bs4 import BeautifulSoup
+
+# question PostTypeId="1" must have AcceptedAnswerId
+    # question PostTypeId="2"
+    # list_question -> question ID, test question, Tags= &lt;java&gt;
+    # list_answer -> find(parent_id=question_id)
+
+# format JSON:
+# {"list_string": []}
+
+
+class DataCollector:
+
+    def __init__(self, file_path="data/Posts.xml"):
+        root = lxml.etree.parse(file_path)
+
+        self.list_question = root.xpath("row[contains(@Tags, '<java>') and @AcceptedAnswerId]")
+        self.list_answer = []
+
+        for question in self.list_question:
+            list_answer_from_parent = root.xpath("row[@ParentId=\"" + question.attrib.get("Id") + "\"]")
+            for answer in list_answer_from_parent:
+                self.list_answer.append(answer)
+
+    def start_data_collection(self):
+
+        dict_save = {"list_string": []}
+
+        for question in self.list_question:
+            dict_save["list_string"].append(BeautifulSoup(question.attrib.get("Body")).text)
+
+        for answer in self.list_answer:
+            dict_save["list_string"].append(BeautifulSoup(answer.attrib.get("Body")).text)
+
+        with open('data.json', 'w') as outfile:
+            json.dump(dict_save, outfile)
---
+++ start.py
@@ -0,0 +1,5 @@
+from dataset_collection.collector_data import DataCollector
+
+if __name__ == "__main__":
+    data_collector = DataCollector()
+    data_collector.start_data_collection()
