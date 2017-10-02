0795e6d7e914edf3f11301a57621ac8ae4ffb35c
kenfrand <kenfrand@gmail.com>
2017-10-02 14:32:22 +0800

    Adde sorting

--- dataset_analysis/stemming.py
+++ dataset_analysis/stemming.py
@@ -26,6 +26,9 @@ def stem_data(data_dir):
             else:
                 stemmed_word_count[parsed] = [1, [element]]
 
+    stemmed_word_count = sorted(stemmed_word_count.items(), key=lambda count: count[1][0])
+    word_count = sorted(word_count.items(), key=lambda count: count[1][0])
+
     with open(data_dir + 'result_stemmed.json', 'w') as result:
         json.dump(stemmed_word_count, result)
 
