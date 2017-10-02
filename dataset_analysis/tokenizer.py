07955e6a69ce25cca8ea10c6f269b692736b0d69
AssignmentNTU <edwardsujono81@gmail.com>
2017-10-03 02:05:55 +0800

    finish the tokenizer, can udate the regex easily

---
+++ dataset_analysis/tokenizer.py
@@ -0,0 +1,35 @@
+import re
+import nltk
+
+
+class Tokenizer:
+
+    def __init__(self):
+        return
+
+    def start_tokenize(self, string):
+
+        list_regex_exception_token = ["(\Exception(.||\\n)*([^:,\s]+:\s+))", "(\java\.\lang\.[a-zA-Z]*\Exception{1})"]
+
+        set_string_match = set()
+        list_string_token = []
+
+        for regex_pattern in list_regex_exception_token:
+
+            m = re.search(regex_pattern, string)
+
+            if m is None:
+                continue
+
+            string_match = m.group(0)
+            set_string_match.add(string_match)
+            string = string.replace(string_match, "").strip()
+            list_string_token.append({string_match: "EXCEPTION"})
+
+        sentence_token = nltk.word_tokenize(string)
+
+        for sen_token in sentence_token:
+
+            list_string_token.append({sen_token: sen_token})
+
+        print list_string_token
--- start.py
+++ start.py
@@ -1,10 +1,16 @@
 from dataset_collection.collector_data import DataCollector
 from dataset_analysis.pos_tagging import pos_tag
 from dataset_analysis.stemming import stem_data
+from dataset_analysis.tokenizer import Tokenizer
+
 
 if __name__ == "__main__":
-    data_collector = DataCollector()
-    data_collector.start_data_collection()
+    # data_collector = DataCollector()
+    # data_collector.start_data_collection()
 
-    pos_tag("data/")
-    stem_data("data/")
+    # pos_tag("data/")
+    # stem_data("data/")
+
+    tokenizer = Tokenizer()
+    tokenizer.start_tokenize("ERROR java.lang.NullPointerException: Sample Java Logback Exception this is test from edward")
+
