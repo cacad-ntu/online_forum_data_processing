650573fe20a4ed8783d830dd378ddbc68115695f
cacad101 <gbbpentium@gmail.com>
2017-11-02 11:01:17 +0800

    add script to count token

---
+++ further_analysis/count_tokenizer.py
@@ -0,0 +1,44 @@
+"""
+Count and sort result of the new tokenizer
+"""
+
+import json
+import operator
+
+# from tokenizer.Regex import Tokenizer
+
+def count_token(data_dir):
+    """ get token count using new tokenizer """
+
+    # re_tokenizer = Tokenizer()
+    # re_tokenizer.start_tokenize_from_folder(data_dir)
+
+    with open(data_dir + 'list_string_regex_token.json') as data_file:
+        data = json.load(data_file)
+
+    with open(data_dir + "stop_words.json") as stop_files:
+        stop_words = json.load(stop_files)
+
+    word_count = {}
+
+    for item in data:
+        for element in item:
+            for token, tag in element.items():
+                if tag.upper() != "JAVA":
+                    continue
+
+                if token.lower() in stop_words:
+                    continue
+
+                if token in word_count:
+                    word_count[token] += 1
+                else:
+                    word_count[token] = 1
+
+    word_count = sorted(word_count.items(), key=operator.itemgetter(1), reverse=True)
+
+    with open(data_dir + 'result_new_token_count.json', 'w') as result:
+        json.dump(word_count, result, indent=4)
+
+if __name__ == "__main__":
+    count_token("../data/")
